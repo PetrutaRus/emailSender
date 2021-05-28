7ee4dc2cf579a9da05c2ec7dce4e7a0dfe0a7eab
Rus Petruta Emilia <58035147+PetrutaRus@users.noreply.github.com>
2021-05-28 21:47:09 +0300

    Delete Code/Spring/BikeRegistration/src/main directory

--- Code/Spring/BikeRegistration/src/main/java/com/example/bikeRegistration/repository/BikeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bikeRegistration.repository;
-
-import com.example.bikeRegistration.entities.Bike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BikeRepository extends JpaRepository<Bike, Long > {
-    Bike findById(long id);
-
-}
